da3b301a01e79f088161cafc33d5924cc8b2292b
Miles Erickson <miles.erickson@gmail.com>
2017-04-17 21:27:18 -0700

    Update Day 2 source

--- day2/src/dict_exercise.py
+++ day2/src/dict_exercise.py
@@ -1,4 +1,8 @@
-def dict_to_text(input_dict):
+#!/usr/bin/env python
+"""Exercises to attain mastery of Python dictionaries."""
+
+
+def dict_to_str(input_dict):
     """Return a str containing each key and value in dict d.
 
     Keys and values are separated by a colon and a space.
@@ -11,37 +15,49 @@ def dict_to_text(input_dict):
     Returns
     -------
     output_text : str
-
-    Examples
-    -------
-    >>> dict_to_str({'a': 1, 'b': 2})
-    "a: 1\nb: 2"
     """
     pass
 
 
 def dict_to_str_sorted(d):
-    '''
-    INPUT: dict
-    OUTPUT: str
+    """Return an ordered str containing each key and value in dict d.
 
-    Return a str containing each key and value in dict d. Keys and values are
-    separated by a colon and a space. Each key-value pair is sorted in ascending order by key.
     This is sorted version of dict_to_str().
 
+    Keys and values are separated by a colon and a space.
+    Each key-value pair is sorted in ascending order by key.
+
     Note: This one is also doable in one line!
-    '''
+
+    Parameters
+    ----------
+    input_dict : dict
+
+    Returns
+    -------
+    output_text : str
+    """
     pass
 
 
 def dict_difference(d1, d2):
-    '''
-    INPUT: dict, dict
-    OUTPUT: dict
+    """Combine two dictionaries according to a specified set of rules.
 
-    Combine the two dictionaries, d1 and d2 as follows. The keys are the union of the keys
-    from each dictionary. If the keys are in both dictionaries then the values should be the
-    absolute value of the difference between the two values. If a value is only in one dictionary, the
-    value should be the absolute value of that value.
-    '''
+    Combine the two dictionaries, d1 and d2, as follows:
+        1. The keys are the union of the keys from each dictionary.
+        2. If a key is in both dictionaries, then the value for that
+           key should be the absolute value of the difference between
+           the two values for that key.
+        3. If a key is only in one dictionary, the value should be the
+           absolute value of that value for that key.
+
+    Parameters
+    ----------
+    d1 : dict
+    d2 : dict
+
+    Returns
+    -------
+    dict
+    """
     pass
--- day2/src/efficiency.py
+++ day2/src/efficiency.py
@@ -1,49 +1,71 @@
+#!/usr/bin/env python
+"""Exercises to practice improving the efficiency of existing Python code."""
+
+
 from string import punctuation
 
 
-def invalid_words(wordlist, document_filename):
-    '''
-    INPUT: list, str
-    OUTPUT: int
+def count_invalid_words(valid_words, document_filename):
+    """Return a list of invalid words found in the specified file.
+
+    Parameters
+    ----------
+    valid_words : list
+    document_filename : str
+
+    Returns
+    -------
+    invalid_words : list
 
     Given a list of all the valid words and a document filename, return a list
     of words from the document that are not valid words.
-    '''
+    """
     with open(document_filename) as doc:
         result = []
         for line in doc:
             words = line.strip().split()
             for word in words:
-                if word.lower().strip(punctuation) not in wordlist:
+                if word.lower().strip(punctuation) not in valid_words:
                     result.append(word)
         return result
 
 
-def common_characters(s, num):
-    '''
-    INPUT: str, int
-    OUTPUT: list of chars
+def find_common_characters(input_string, num):
+    """Return a list of characters appearing frequently in input_string.
 
-    Return the list of characters which appear in the string s more than num
-    times.
-    '''
-    result = []
-    for char in s:
-        if s.count(char) > num:
+    Parameters
+    ----------
+    input_string : str
+    num : int
+
+    Returns
+    -------
+    common_characters : list
+        List of characters appearing more than num times in input_string.
+    """
+    common_characters = []
+    for char in input_string:
+        if input_string.count(char) > num:
             if char not in result:
-                result.append(char)
+                common_characters.append(char)
     return result
 
 
 def sum_to_zero(lst):
-    '''
-    INPUT: list of ints
-    OUTPUT: tuple of two ints
+    """Return a tuple of two values from the input list that sum to zero.
 
-    Return a tuple of two values from the input list that sum to zero.
-    If none exist, return None.
-    '''
+    If no such pair of values exists, return None.
 
+    Parameters
+    ----------
+    lst: list
+        List of ints.
+
+    Returns
+    -------
+    value_pair: tuple
+        Two integers.
+    """
     for i, item1 in enumerate(lst):
         for item2 in lst[i + 1:]:
             if item1 + item2 == 0:
